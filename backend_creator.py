35124fec51e5fce550eafe3d958f73af9a5a436f
bmintz <bmintz@users.noreply.github.com>
2018-02-13 02:49:27 -0600

    backend_creator.py: support resumption, and minor fixes

--- backend_creator.py
+++ backend_creator.py
@@ -4,19 +4,21 @@
 """a selfbot that creates guilds"""
 
 from functools import wraps
+import os
+import sys
 import time
 
 import discord
 import gi
-gi.require_version('Notify', '0.7')  # warnings if I don't do this ¯⧹_(ツ)_⧸¯
+gi.require_version('Notify', '0.7')  # warnings if I don't do this ¯\_(ツ)_/¯
 from gi.repository import Notify
 import selenium.webdriver
 from selenium.common.exceptions import NoSuchElementException
 
-from bot import EmojiConnoisseur
+from db import CONFIG
+
 
 bot = discord.Client(self_bot=True)
-connoisseur = EmojiConnoisseur()
 Notify.init(__name__)
 
 
@@ -27,14 +29,14 @@ def print_status(status_message, synchronous=False):
 			def wrapped(*args, **kwargs):
 				print('\n' + status_message + '...', end=' ')
 				result = func(*args, **kwargs)
-				print('done.')
+				print('done.', end='')
 				return result
 		else:
 			@wraps(func)
 			async def wrapped(*args, **kwargs):
 				print('\n' + status_message + '...', end=' ')
 				result = await func(*args, **kwargs)
-				print('done.')
+				print('done.', end='')
 				return result
 		return wrapped
 	return wrapper
@@ -44,11 +46,11 @@ def print_status(status_message, synchronous=False):
 async def on_ready():
 	print('Ready.')
 	#await wipe_guilds()
-	#await create_guilds(prefix='Emoji Backend ', limit=100)
+	#await create_guilds(prefix='EmojiBackend ', limit=100)
 	#await clear_guilds()
 	#await rename_guilds()
 	add_bot_to_guilds()
-	#await bot.logout()
+	await bot.logout()
 	return
 
 
@@ -59,29 +61,23 @@ async def wipe_guilds():
 
 
 @print_status('Creating guilds')
-async def create_guilds(prefix, start=0, limit=50):
+async def create_guilds(prefix, start=0, limit=100):
 	"""create at most `limit` guilds named with numbers starting at `start`"""
 
 	for i in range(start, limit):
-		await bot.create_guild(prefix + str(i))
+		# space out the number so that the icon for each guild in the sidebar shows the full number
+		await bot.create_guild(prefix + ' '.join(str(i)))
 
 
-@print_status('Clearing guilds')
+@print_status('Clearing default channels')
 async def clear_guilds():
 	for guild in bot.guilds:
 		# By default, discord creates 4 channels to make it easy for users:
 		# A "text channels" category, a "voice channels" category,
 		# a voice channel and a text channel. We want none of those.
+		# There is also an invite created for the text channel, but that's deleted when the channel dies.
 		for channel in guild.channels:
 			await channel.delete()
-		for invite in await guild.invites():
-			await invite.delete()
-
-
-@print_status('Renaming guilds')
-async def rename_guilds():
-	for guild in bot.guilds:
-		await guild.edit(name=guild.name.replace('Emoji', 'EmojiBackend'))
 
 
 def wait_for_element(driver, css_selector, delay=0.25):
@@ -99,16 +95,23 @@ def wait_for_url(driver, url, delay=0.25):
 
 @print_status('Adding bot to guilds. This will require your input', synchronous=True)
 def add_bot_to_guilds():
+	# these are the guilds which do not have the bot in them yet
+	pending_guilds = [guild for guild in bot.guilds if guild.member_count == 1]
+
 	driver = selenium.webdriver.Firefox()
 	perms = discord.Permissions.none()
 	perms.manage_emojis = True
-	oauth_url = discord.utils.oauth_url(connoisseur.client_id, perms)
+	# the client ID is not always the same as the user id.
+	# however, they're only different on older bot accounts.
+	# normally, i would use client_id but that can't be retrieved until the bot is ready,
+	# which means the bot has to be running...
+	oauth_url = discord.utils.oauth_url(CONFIG['client_id'], perms)
 	driver.get(oauth_url)
 
 	notify('Waiting for you to log in')
 	wait_for_url(driver, oauth_url) # oauth URL redirects to login page if logged out
 
-	for i, guild in enumerate(bot.guilds, 1):
+	for i, guild in enumerate(pending_guilds, 1):
 		driver.get(oauth_url)
 
 		wait_for_element(driver, 'select > option[value="{}"]'.format(guild.id)).click()
@@ -117,22 +120,30 @@ def add_bot_to_guilds():
 		wait_for_element(driver, '.recaptcha-checkbox-checkmark').click()
 		driver.switch_to.default_content() # switch back out
 		wait_for_url(driver, 'https://discordapp.com/oauth2/authorized')
-		notify('{} guild{} down, {} to go!'.format(i, 's' if i > 1 else '', len(bot.guilds) - i))
+		notify('{} guild{} down, {} to go!'.format(i, 's' if i > 1 else '', len(pending_guilds) - i))
 
 
 def notify(message):
 	Notify.Notification.new(message).show()
 
 
-def main():
-	import os
-	import sys
+def usage():
+	print('Usage:', sys.argv[0], '<token>', file=sys.stderr)
+	print('You can also set $token.', file=sys.stderr)
+	sys.exit(1)
 
-	try:
-		bot.run(os.getenv('token', sys.argv[1]), bot=False)
-	except IndexError:
-		print('Usage:', sys.argv[0], '<token>', file=sys.stderr)
-		print('You can also set $token.')
+
+def main():
+	if len(sys.argv) > 1:
+		token = sys.argv[1]
+	elif len(sys.argv) == 1:
+		token = os.getenv('token')
+		if token is None:
+			usage()
+	else:
+		usage()
+
+	bot.run(token, bot=False)
 
 
 if __name__ == '__main__':
