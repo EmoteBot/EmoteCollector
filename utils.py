86a1a20b535ae3ad78e2b74509c5fbdecfc76105
bmintz <bmintz@users.noreply.github.com>
2018-02-17 18:51:12 -0600

    Revert "utils: remove unneeded functions"
    This reverts commit efd9c19ce33425e6058698f8ebb464337861ba9b.
    We need the typing decorator again, since apparently context.trigger_typing() is only for 10s

--- utils.py
+++ utils.py
@@ -11,6 +11,14 @@ from aiohttp import ClientSession as _ClientSession
 session = _ClientSession()
 
 
+def typing(func):
+	@_wraps(func)
+	async def wrapped(self, context, *args, **kwargs):
+		async with context.typing():
+			await func(self, context, *args, **kwargs)
+	return wrapped
+
+
 async def create_gist(filename, contents: str):
 	"""Upload a single file to Github Gist. Multiple files Never™"""
 	async with session.post(
@@ -22,3 +30,8 @@ async def create_gist(filename, contents: str):
 					'content': contents}}})) as resp:
 		if resp.status == 201:
 			return _json.loads(await resp.text())['html_url']
+
+
+def log(*args, **kwargs):
+	kwargs['file'] = _sys.stderr
+	print(*args, **kwargs)
