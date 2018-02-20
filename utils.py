3c0e2ff2453b2089daf16c1049da95cbccef6d72
bmintz <bmintz@users.noreply.github.com>
2018-02-20 03:41:20 -0600

    utils: reorganize

--- utils.py
+++ utils.py
@@ -13,6 +13,36 @@ session = _ClientSession()
 _sentinel = object()
 
 
+def typing(func):
+	"""Makes a command function run with the context.typing() decorator.
+	This will make the bot appear to be typing for until the command returns.
+	While you can just wrap your entire code in `async with context.typing()`,
+	this isn't ideal if you already have a lot of indents or a long function.
+	Also, context.trigger_typing() works but only for 10 seconds.
+	"""
+	@_wraps(func)
+	async def wrapped(*args, **kwargs):
+		# if func is a method, args starts with self, context, ...
+		# otherwise args starts with context, ...
+		context = args[0] if isinstance(args[0], commands.Context) else args[1]
+		async with context.typing():
+			await func(*args, **kwargs)
+	return wrapped
+
+
+async def create_gist(filename, contents: str):
+	"""Upload a single file to Github Gist. Multiple files Never™"""
+	async with session.post(
+		'https://api.github.com/gists',
+		data=_json.dumps({
+			'public': False,
+			'files': {
+				filename: {
+					'content': contents}}})) as resp:
+		if resp.status == 201:
+			return _json.loads(await resp.text())['html_url']
+
+
 class LRUDict:
 	"""An extension of lru.LRU to add `pop`"""
 
@@ -92,33 +122,3 @@ class LRUDict:
 
 	def __contains__(self, key):
 		return key in self._dict
-
-
-def typing(func):
-	"""Makes a command function run with the context.typing() decorator.
-	This will make the bot appear to be typing for until the command returns.
-	While you can just wrap your entire code in `async with context.typing()`,
-	this isn't ideal if you already have a lot of indents or a long function.
-	Also, context.trigger_typing() works but only for 10 seconds.
-	"""
-	@_wraps(func)
-	async def wrapped(*args, **kwargs):
-		# if func is a method, args starts with self, context, ...
-		# otherwise args starts with context, ...
-		context = args[0] if isinstance(args[0], commands.Context) else args[1]
-		async with context.typing():
-			await func(*args, **kwargs)
-	return wrapped
-
-
-async def create_gist(filename, contents: str):
-	"""Upload a single file to Github Gist. Multiple files Never™"""
-	async with session.post(
-		'https://api.github.com/gists',
-		data=_json.dumps({
-			'public': False,
-			'files': {
-				filename: {
-					'content': contents}}})) as resp:
-		if resp.status == 201:
-			return _json.loads(await resp.text())['html_url']
