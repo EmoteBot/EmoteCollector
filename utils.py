fb9aeee0cbe2f4ffd0d3099e412b2d1991b1d341
bmintz <bmintz@users.noreply.github.com>
2018-02-19 18:29:43 -0600

    utils.typing: update docstring

--- utils.py
+++ utils.py
@@ -23,11 +23,14 @@ def pop(d, key, default=None):
 def typing(func):
 	"""Makes a command function run with the context.typing() decorator.
 	This will make the bot appear to be typing for until the command returns.
-	The function must be in a class, as self is assumed to be the first parameter, and context
-	is assumed to be the second."""
+	While you can just wrap your entire code in `async with context.typing()`,
+	this isn't ideal if you already have a lot of indents or a long function.
+	Also, context.trigger_typing() works but only for 10 seconds.
+	"""
 	@_wraps(func)
 	async def wrapped(*args, **kwargs):
-		# if func is a method, args[0] will be self, otherwise it'll be a context
+		# if func is a method, args starts with self, context, ...
+		# otherwise args starts with context, ...
 		context = args[0] if isinstance(args[0], commands.Context) else args[1]
 		async with context.typing():
 			await func(*args, **kwargs)
