38a5453aa8dac45ffb0539eff3cb99de6d14e194
bmintz <bmintz@users.noreply.github.com>
2018-02-23 00:09:09 -0600

    cogs/stats.py: use correct cog name

--- cogs/stats.py
+++ cogs/stats.py
@@ -7,10 +7,10 @@ from .external.stats import Stats
 
 class EmojiConnoisseurStats(Stats):
 	async def guild_count(self):
-		emoji_cog = self.bot.get_cog('Emoji')
-		while emoji_cog is None or not hasattr(emoji_cog, 'guilds'):
+		emote_cog = self.bot.get_cog('Emotes')
+		while emote_cog is None or not hasattr(emote_cog, 'guilds'):
 			await asyncio.sleep(0.1)
-		return super().guild_count - len(emoji_cog.guilds)
+		return super().guild_count - len(emote_cog.guilds)
 
 
 def setup(bot):
