94b06551874185b924dd63d9d6d06bbc06bc1e8d
bmintz <bmintz@users.noreply.github.com>
2018-02-22 23:53:47 -0600

    cogs/stats.py: fail if emote cog hasn't loaded

--- cogs/stats.py
+++ cogs/stats.py
@@ -7,9 +7,7 @@ class EmojiConnoisseurStats(Stats):
 	@property
 	def guild_count(self):
 		emoji_cog = self.bot.get_cog('Emoji')
-		# fallback in case the emotes cog hasn't been loaded yet or its on_ready hasn't run yet
-		backend_guild_count = 0 if emoji_cog is None else len(emoji_cog.guilds)
-		return super().guild_count - backend_guild_count
+		return super().guild_count - len(emoji_cog.guilds)
 
 
 def setup(bot):
