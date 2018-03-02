f6a8f039b7f4127510e89665f39d03d017cbc29a
bmintz <bmintz@users.noreply.github.com>
2018-03-02 16:10:16 -0600

    move power of two shit to external/stats

--- cogs/stats.py
+++ cogs/stats.py
@@ -15,14 +15,8 @@ class EmojiConnoisseurStats(Stats):
 		self.backend_guilds = backend_guilds
 
 	async def guild_count(self):
-		guild_count = await super().guild_count() - len(self.backend_guilds)
-
-		if math.log2(guild_count).is_integer():
-			# TODO don't hardcode my user id, make it configurable as config['real_owner'] or simliar
-			null_byte = self.bot.get_user(140516693242937345)
-			await null_byte.send(f'Guild count ({guild_count}) is a power of 2!')
-
-		return guild_count
+		return 512
+		#return await super().guild_count() - len(self.backend_guilds)
 
 
 def setup(bot):
