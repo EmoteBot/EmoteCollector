5fbf376df4ae757b19e1e46f4ee3b0dcaa14993a
bmintz <bmintz@users.noreply.github.com>
2018-03-06 21:14:17 -0600

    utils: add checks module

--- utils/checks.py
+++ utils/checks.py
@@ -1,2 +1,17 @@
 #!/usr/bin/env python3.6.6
 # encoding: utf-8
+
+from discord.ext import commands
+
+
+# Used under the MIT license. Copyright (c) 2017 BeatButton
+# https://github.com/BeatButton/beattie/blob/44fd795aef7b1c19233510cda8046baab5ecabf3/utils/checks.py
+def owner_or_permissions(**perms):
+	"""Checks if the member is a bot owner or has any of the permissions necessary."""
+	async def predicate(ctx):
+		if await ctx.bot.is_owner(ctx.author):
+			return True
+		permissions = ctx.channel.permissions_for(ctx.author)
+		return any(getattr(permissions, perm, None) == value
+				   for perm, value in perms.items())
+	return commands.check(predicate)
