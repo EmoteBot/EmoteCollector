4638a3e1ffd59d8549538eacc6ed6ddd1aa11bb0
bmintz <bmintz@users.noreply.github.com>
2018-03-08 00:22:18 -0600

    cogs/stats: improve memory usage

--- cogs/stats.py
+++ cogs/stats.py
@@ -5,15 +5,15 @@ from .external.stats import Stats
 
 class Stats(Stats):
 	def __init__(self, bot):
-		self.backend_guilds = []
+		self.backend_guilds = 0
 		super().__init__(bot)
 
 	async def on_backend_guild_enumeration(self, backend_guilds):
-		self.backend_guilds = backend_guilds
+		self.backend_guilds = len(backend_guilds)
 
 	async def guild_count(self):
-		return await super().guild_count() - len(self.backend_guilds)
+		return await super().guild_count() - self.backend_guilds
 
 
 def setup(bot):
-	bot.add_cog(EmojiConnoisseurStats(bot))
+	bot.add_cog(Stats(bot))
