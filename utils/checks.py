10a8d3df7fac2dd20cbd803ae8abaf38c12cbee8
Ben Mintz <bmintz@protonmail.com>
2018-03-18 21:13:42 -0500

    utils/checks.py: rework logic to reduce indents

--- utils/checks.py
+++ utils/checks.py
@@ -21,10 +21,10 @@ def not_blacklisted():
 	async def predicate(context):
 		db = context.cog.db
 		blacklist_reason = await db.get_user_blacklist(context.author.id)
-		if blacklist_reason is not None:
-			await context.send(
-				f'Sorry, you have been blacklisted with the reason `{blacklist_reason}`. '
-				f'To appeal, please join the support server with `{context.prefix}support`.')
-			return False
-		return True
+		if blacklist_reason is None:
+			return True
+		await context.send(
+			f'Sorry, you have been blacklisted with the reason `{blacklist_reason}`. '
+			f'To appeal, please join the support server with `{context.prefix}support`.')
+		return False
 	return commands.check(predicate)
