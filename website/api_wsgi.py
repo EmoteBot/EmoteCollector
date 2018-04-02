ee65c8c036db043f21609870f2f19b83fe518cea
bmintz <bmintz@users.noreply.github.com>
2018-04-02 13:39:32 -0500

    api: add WSGI stuff

---
+++ website/api_wsgi.py
@@ -0,0 +1,6 @@
+# encoding: utf-8
+
+from api import app as application
+
+if __name__ == '__main__':
+	application.run(debug=False)
---
+++ website/start-api.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env sh
+gunicorn --workers 1 --threads 4 --max-requests 10000 --reload --bind 0.0.0.0:8081 api_wsgi
