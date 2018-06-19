9982f81876ead45f2011e91b65d6de9660b60d3c
bmintz <bmintz@protonmail.com>
2018-06-18 20:03:08 -0500

    remove start-* files in favor of one

--- website/start-app.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env sh
-gunicorn --workers 1 --threads 4 --max-requests 10000 --reload --bind 0.0.0.0:8080 app:app
---
+++ website/start.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+
+gunicorn --workers 1 --threads 4 --max-requests 10000 --reload --bind 0.0.0.0:8080 app:app &
+gunicorn --workers 1 --threads 4 --max-requests 10000 --reload --bind 0.0.0.0:8081 api:app &
+
+wait
