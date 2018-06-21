dc9fd1976d9eb96ac0e5dd2fd692d7e8892ef0db
bmintz <bmintz@protonmail.com>
2018-06-21 13:54:31 -0500

    move SQL schema to separate file

--- cogs/db.py
+++ cogs/db.py
@@ -7,6 +7,7 @@ import logging
 import random
 import time
 
+import aiofiles
 import asyncpg
 import discord
 from discord.ext import commands
@@ -394,51 +395,8 @@ class Database:
 			await self.bot.logout()
 			return
 
-		await db.execute('SET TIME ZONE UTC')  # make sure timestamps are displayed correctly
-		await db.execute('CREATE SCHEMA IF NOT EXISTS connoisseur')
-		await db.execute("""
-			CREATE TABLE IF NOT EXISTS emojis(
-				name VARCHAR(32) NOT NULL,
-				id BIGINT NOT NULL UNIQUE,
-				author BIGINT NOT NULL,
-				animated BOOLEAN DEFAULT FALSE,
-				description VARCHAR(280),
-				created TIMESTAMP WITH TIME ZONE DEFAULT (now() at time zone 'UTC'),
-				modified TIMESTAMP WITH TIME ZONE,
-				preserve BOOLEAN DEFAULT FALSE)""")
-		await db.execute('CREATE UNIQUE INDEX IF NOT EXISTS emojis_lower_idx ON emojis (LOWER(name))')
-		await db.execute("""
-			-- https://stackoverflow.com/a/26284695/1378440
-			CREATE OR REPLACE FUNCTION update_modified_column()
-			RETURNS TRIGGER AS $$
-			BEGIN
-				IF row(NEW.*) IS DISTINCT FROM row(OLD.*) THEN
-					NEW.modified = now() at time zone 'UTC';
-					RETURN NEW;
-				ELSE
-					RETURN OLD;
-				END IF;
-			END;
-			$$ language 'plpgsql';""")
-		await db.execute('DROP TRIGGER IF EXISTS update_emoji_modtime ON emojis')
-		await db.execute("""
-			CREATE TRIGGER update_emoji_modtime
-			BEFORE UPDATE ON emojis
-			FOR EACH ROW EXECUTE PROCEDURE update_modified_column();""")
-		await db.execute('DROP TABLE IF EXISTS blacklists')
-		await db.execute("""
-			CREATE TABLE IF NOT EXISTS user_opt(
-				id BIGINT NOT NULL UNIQUE,
-				state BOOLEAN,
-				blacklist_reason VARCHAR(500))""")
-		await db.execute("""
-			CREATE TABLE IF NOT EXISTS guild_opt(
-				id BIGINT NOT NULL UNIQUE,
-				state BOOLEAN NOT NULL)""")
-		await db.execute("""
-			CREATE TABLE IF NOT EXISTS emote_usage_history(
-				id BIGINT REFERENCES emojis (id),
-				time TIMESTAMP WITH TIME ZONE DEFAULT (now() at time zone 'UTC'))""")
+		async with aiofiles.open('data/schema.sql') as f:
+			await db.execute(await f.read())
 
 		self.db = db  # pylint: disable=invalid-name
 
---
+++ data/schema.sql
@@ -0,0 +1,47 @@
+SET TIME ZONE UTC;
+
+CREATE TABLE IF NOT EXISTS emojis(
+	name VARCHAR(32) NOT NULL,
+	id BIGINT NOT NULL UNIQUE,
+	author BIGINT NOT NULL,
+	animated BOOLEAN DEFAULT FALSE,
+	description VARCHAR(280),
+	created TIMESTAMP WITH TIME ZONE DEFAULT (now() at time zone 'UTC'),
+	modified TIMESTAMP WITH TIME ZONE,
+	preserve BOOLEAN DEFAULT FALSE);
+
+CREATE UNIQUE INDEX IF NOT EXISTS emojis_lower_idx ON emojis (LOWER(name));
+
+-- https://stackoverflow.com/a/26284695/1378440
+CREATE OR REPLACE FUNCTION update_modified_column()
+RETURNS TRIGGER AS $$
+BEGIN
+	IF row(NEW.*) IS DISTINCT FROM row(OLD.*) THEN
+		NEW.modified = now() at time zone 'UTC';
+		RETURN NEW;
+	ELSE
+		RETURN OLD;
+	END IF;
+END;
+$$ language 'plpgsql';
+
+DROP TRIGGER IF EXISTS update_emoji_modtime ON emojis;
+
+CREATE TRIGGER update_emoji_modtime
+BEFORE UPDATE ON emojis
+FOR EACH ROW EXECUTE PROCEDURE update_modified_column();
+
+DROP TABLE IF EXISTS blacklists;
+
+CREATE TABLE IF NOT EXISTS user_opt(
+	id BIGINT NOT NULL UNIQUE,
+	state BOOLEAN,
+	blacklist_reason VARCHAR(500));
+
+CREATE TABLE IF NOT EXISTS guild_opt(
+	id BIGINT NOT NULL UNIQUE,
+	state BOOLEAN NOT NULL);
+
+CREATE TABLE IF NOT EXISTS emote_usage_history(
+	id BIGINT REFERENCES emojis (id),
+	time TIMESTAMP WITH TIME ZONE DEFAULT (now() at time zone 'UTC'));
