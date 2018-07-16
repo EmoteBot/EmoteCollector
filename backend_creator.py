a1d6c8d8798f70d442d007fe1b33ae72cb1e3ca4
bmintz <bmintz@protonmail.com>
2018-07-16 00:00:48 -0500

    update the backend creator

--- backend_creator.py
+++ backend_creator.py
@@ -3,41 +3,37 @@
 
 """a selfbot that creates guilds"""
 
-import asyncio
-import base64
-from functools import wraps
-import json
 import os
 import sys
 import time
+import base64
+import typing
+import asyncio
+import inspect
 
 import discord
-import gi
-gi.require_version('Notify', '0.7')  # warnings if I don't do this ¯\_(ツ)_/¯
-from gi.repository import Notify
-import selenium.webdriver
-from selenium.common.exceptions import NoSuchElementException
+
+import utils
 
 
 bot = discord.Client(self_bot=True)
-Notify.init(__name__)
 
 
-def print_status(status_message, synchronous=False):
+def print_status(status_message):
 	def wrapper(func):
-		if asyncio.iscoroutinefunction(func):
+		if inspect.iscoroutinefunction(func):
 			# @wraps doesn't work on coros
 			async def wrapped(*args, **kwargs):
-				print('\n' + status_message + '...', end=' ')
+				print(status_message + '...', end=' ', file=sys.stderr)
 				result = await func(*args, **kwargs)
-				print('done.', end='')
+				print('done.', file=sys.stderr)
 				return result
 		else:
 			@wraps(func)
 			def wrapped(*args, **kwargs):
-				print('\n' + status_message + '...', end=' ')
+				print(status_message + '...', end=' ', file=sys.stderr)
 				result = func(*args, **kwargs)
-				print('done.', end='')
+				print('done.', file=sys.stderr)
 				return result
 		return wrapped
 	return wrapper
@@ -47,11 +43,15 @@ def print_status(status_message, synchronous=False):
 async def on_ready():
 	print('Ready.')
 	await wipe_guilds()
-	await create_guilds(prefix='EmojiBackend ', limit=100)
+	await create_guilds(prefix='EmoteBackend ', limit=100)
 	await clear_guilds()
-	# await rename_guilds()
-	add_bot_to_guilds()
-	await bot.logout()
+	await rename_guilds()
+
+	global needed_guilds
+	bot.needed_guilds = {guild for guild in bot.guilds if len(guild.members) < 2}
+
+	await add_bot_to_guilds()
+
 	return
 
 
@@ -86,79 +86,58 @@ async def clear_guilds():
 
 @print_status('Renaming guilds')
 async def rename_guilds():
-	for guild in bot.guilds:
-		await guild.edit(name=guild.name.replace('Emoji Backend', 'EmojiBackend'))  # example
+	for i, guild in enumerate(bot.guilds, 100):
+		await guild.edit(name='EmojiBackend %s' % ' '.join(str(i)))
 
 
-@print_status('Adding bot to guilds. This will require your input', synchronous=True)
-def add_bot_to_guilds():
-	# these are the guilds which do not have the bot in them yet
-	pending_guilds = [guild for guild in bot.guilds if guild.member_count == 1]
+async def add_bot_to_guilds():
+	try:
+		next_guild = get_needed_guild()
+	except ValueError:
+		print(
+			'Either all the backend guilds have been added to the bot,',
+			'or you have not entered the user token correctly.',
+			'Make sure that the user token you entered is the same one that owns all the backend guilds.')
+		await bot.logout()
+		return
 
-	driver = selenium.webdriver.Firefox()
-
-	perms = discord.Permissions()
-	perms.manage_emojis = True
-
-	# XXX for older accounts, a bot's user ID
-	# is not the same as its oauth client ID.
-	# if that is your situation, adjust the code below
-	client_id = token_id(get_token_from_config())
-	oauth_url = discord.utils.oauth_url(client_id, perms)
-
-	driver.get(oauth_url)
-
-	notify('Waiting for you to log in')
-	wait_for_url(driver, oauth_url)  # oauth URL redirects to login page if logged out
-
-	for i, guild in enumerate(pending_guilds, 1):
-		driver.get(oauth_url)
-
-		add_guild(driver, guild)
-		notify('{} guild{} down, {} to go!'.format(i, 's' if i > 1 else '', len(pending_guilds) - i))
+	print('To add the bot to the backend guilds, use this link:')
+	needed_permissions = discord.Permissions()
+	needed_permissions.update(manage_emojis=True)
+	print(discord.utils.oauth_url(get_bot_user_id(), needed_permissions))
+	print('Then add the bot to this guild:', next_guild.name)
 
 
-def add_guild(driver, guild):
-	# wait until the dropdown to choose a server appears
-	wait_for_element(driver, 'select > option[value="{}"]'.format(guild.id)).click()
-	wait_for_element(driver, 'button.primary').click()
-	driver.switch_to.frame(wait_for_element(driver, 'iframe'))  # switch to the reCAPTCHA iframe
-	# click "i am not a robot"
-	wait_for_element(driver, '.recaptcha-checkbox-checkmark').click()
-	driver.switch_to.default_content()  # switch back out
-	# wait until the user completes the CAPTCHA and auth is finished
-	wait_for_url(driver, 'https://discordapp.com/oauth2/authorized')
+def get_needed_guild():
+	try:
+		return next(iter(bot.needed_guilds))
+	except StopIteration:
+		raise ValueError('no more guilds') from None
 
+
+@bot.event
+async def on_member_join(member):
+	bot.needed_guilds.remove(member.guild)
+	print(len(bot.guilds) - len(bot.needed_guilds), 'down,', len(bot.needed_guilds), 'to go.', end=' ')
+	try:
+		print('Now add', get_needed_guild().name, end='.\n')
+	except ValueError:
+		pass
+
+
+def get_bot_user_id():
+	return token_id(get_token_from_config())
 
 def get_token_from_config():
-	with open('data/config.json') as f:
-		return json.load(f)['token']
-
+	with open('data/config.py') as f:
+		return utils.load_json_compat(f.read())['tokens']['discord']
 
 def token_id(token) -> int:
 	"""decodes a token to retrieve the user ID"""
 	return int(base64.b64decode(token.split('.')[0]))
 
 
-def wait_for_element(driver, css_selector, delay=0.25):
-	while True:
-		try:
-			return driver.find_element_by_css_selector(css_selector)
-		except NoSuchElementException:
-			time.sleep(delay)
-			continue
-
-
-def wait_for_url(driver, url, delay=0.25):
-	while driver.current_url != url:
-		time.sleep(delay)
-
-
-def notify(message):
-	Notify.Notification.new(message).show()
-
-
-def usage():
+def usage() -> typing.NoReturn:
 	print('Usage:', sys.argv[0], '<token>', file=sys.stderr)
 	print('You can also set $token.', file=sys.stderr)
 	sys.exit(1)
