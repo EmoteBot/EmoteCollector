5b8204803581db76c168a3940df31470339fac8d
bmintz <bmintz@protonmail.com>
2018-09-02 23:44:15 +0000

    greatly simplify __main__ and properly handle Ctrl+C

--- emoji_connoisseur/__main__.py
+++ emoji_connoisseur/__main__.py
@@ -1,21 +1,11 @@
 #!/usr/bin/env python3
 # encoding: utf-8
 
-import asyncio
-import contextlib
-
 from . import EmojiConnoisseur
 from . import utils
-
-loop = asyncio.get_event_loop()
 
 with open('data/config.py') as f:
 	config = utils.load_json_compat(f.read())
 
-bot = EmojiConnoisseur(config=config, loop=loop)
-
-with contextlib.closing(loop):
-	try:
-		loop.run_until_complete(bot.start())
-	finally:
-		loop.run_until_complete(bot.logout())
+bot = EmojiConnoisseur(config=config)
+bot.run()
