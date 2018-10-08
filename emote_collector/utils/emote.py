18bc410458d544c3985670e629380ddb7f9ac6cc
bmintz <bmintz@protonmail.com>
2018-10-08 03:51:49 +0000

    extensions/emote: ignore emoji shortcodes 😒
    sometimes clients send :thinking: instead of 🤔…

--- emote_collector/utils/emote.py
+++ emote_collector/utils/emote.py
@@ -2,8 +2,17 @@
 # encoding: utf-8
 
 """
-various utilities related to custom emotes
+various utilities related to custom emotes and regular emojis
 """
+
+import os.path
+import json
+
+from .. import BASE_DIR
+
+with open(os.path.join(BASE_DIR, 'data', 'discord-emoji-shortcodes.json')) as f:
+	emoji_shortcodes = set(json.load(f))
+del f
 
 def url(id, *, animated: bool = False):
 	"""Convert an emote ID to the image URL for that emote."""
