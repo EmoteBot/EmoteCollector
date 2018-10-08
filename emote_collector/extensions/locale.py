50b585a3fe684c4bd86215e3da8a0b24908f32df
bmintz <bmintz@protonmail.com>
2018-10-08 23:17:41 +0000

    ec/locale: add serverlocale get/set and improve UX of ec/locale
    ec/locale set en_US @lambda#0987 was confusing for two reasons:
    1) typically the order is "destination = value", not the reverse
    2) the member argument was required to set your own locale,
    even though you could not set someone else's.
    
    now the only argument is an optional TextChannel.
    when not provided, the user's locale will be set instead,
    and ec/serverlocale is required to manipulate the server locale.

--- emote_collector/extensions/locale.py
+++ emote_collector/extensions/locale.py
@@ -9,8 +9,6 @@ from discord.ext import commands
 from .. import utils
 from ..utils import i18n
 
-Location = typing.Union[discord.TextChannel, discord.Member]
-
 class Locales:
 	def __init__(self, bot):
 		self.bot = bot
@@ -29,73 +27,63 @@ class Locales:
 		pass
 
 	@locale_command.command(name='get')
-	async def get_locale_command(self, context, location: Location = None):
-		"""Get the locale for a channel, user, or the entire server.
+	async def get_locale_command(self, context, channel: typing.Optional[discord.TextChannel] = None):
+		"""Get the locale for a channel or yourself.
 
-		location: A text channel or user.
-		You can only get your user locale, unless you have the Manage Messages permission.
-		If location is not provided, get the server locale.
+		If a channel is not provided, this command gets your current locale.
 		"""
 
-		if location is None:
-			if context.guild is None:
-				return await context.send(_("You can't get the server locale in DMs."))
+		if channel is None:
+			user_locale = await self.locale(context.message)
+			await context.send(_('Your current locale is: {user_locale}').format(**locals()))
 
-			guild_locale = await self.guild_locale(context.guild.id)
-			if not guild_locale:
-				guild_locale = i18n.default_locale
-			return await context.send(_('The current locale for this server is: {guild_locale}').format(
-				**locals()))
-
-		if isinstance(location, discord.TextChannel):
-			channel_or_guild_locale = await self.channel_or_guild_locale(location)
-			if not channel_or_guild_locale:
-				channel_or_guild_locale = i18n.default_locale
-			return await context.send(_(
+		else:
+			channel_or_guild_locale = await self.channel_or_guild_locale(channel) or i18n.default_locale
+			await context.send(_(
 				'The current locale for that channel is: {channel_or_guild_locale}').format(**locals()))
 
-		if location != context.author and not context.author.guild_permissions.manage_messages:
-			return await context.send(_('You can only get your own locale.'))
-		user_locale = await self.locale(context.message)
-		return await context.send(_('The current locale for that user is: {user_locale}').format(**locals()))
-
 	@locale_command.command(name='set')
-	async def set_locale_command(self, context, locale, location: Location = None):
-		"""Set the locale for a channel, user, or the entire server.
+	async def set_locale_command(self, context, channel: typing.Optional[discord.TextChannel], locale):
+		"""Set the locale for a channel or yourself.
 
-		location: A text channel or user.
-		You can only get your user locale, unless you have the Manage Messages permission.
-		If location is not provided, get the server locale.
+		Manage Messages is required to change the locale of a whole channel.
+		If the channel is left blank, this command sets your user locale.
 		"""
 
 		if locale not in i18n.locales:
 			return await context.send(_('Invalid locale. The valid locales are: {locales}').format(
 				locales=', '.join(i18n.locales)))
 
-		if not isinstance(location, discord.Member) and not context.guild:
-			return await context.send(_('Cannot set the server or channel locale in DMs.'))
+		if channel is None:
+			await self.set_user_locale(context.author.id, locale)
 
-		if (
-			not isinstance(location, discord.Member)
-			and (
-				not context.author.guild_permissions.manage_messages
-				or not await self.bot.is_owner(context.author))
+		elif (
+			not context.author.guild_permissions.manage_messages
+			or not await self.bot.is_owner(context.author)
 		):
 			raise commands.MissingPermissions(('manage_messages',))
 
-		if not location:
-			if not context.guild:
-				return await context.send(_('Cannot set the server locale in DMs.'))
-			await self.set_guild_locale(context.guild.id, locale)
+		else:
+			await self.set_channel_locale(context.guild.id, channel.id, locale)
 
-		if isinstance(location, discord.TextChannel):
-			await self.set_channel_locale(context.guild.id, location.id, locale)
+		await context.try_add_reaction(utils.SUCCESS_EMOJIS[True])
 
-		elif isinstance(location, discord.Member):
-			if context.author != location:
-				return await context.send(_('You cannot set the locale of another user.'))
-			await self.set_user_locale(location.id, locale)
+	@commands.group(name='serverlocale')
+	async def guild_locale_command(self, context):
+		""""Commands relating to modifying the locale.
+		This command does nothing on its own; all functionality is in subcommands.
+		"""
+		pass
 
+	@guild_locale_command.command(name='get')
+	async def get_guild_locale_command(self, context):
+		guild_locale = await self.guild_locale(context.guild.id) or i18n.default_locale
+		await context.send(_('The current locale for this server is: {guild_locale}').format(**locals()))
+
+	@guild_locale_command.command(name='set')
+	@commands.has_permissions(manage_messages=True)
+	async def set_guild_locale_command(self, context, locale):
+		await self.set_guild_locale(context.guild.id, locale)
 		await context.try_add_reaction(utils.SUCCESS_EMOJIS[True])
 
 	async def locale(self, message):
