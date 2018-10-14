ad2774842171a96045237080263eb78bb4640ff5
Benjamin Mintz <bmintz@protonmail.com>
2018-10-14 05:00:21 +0000

    add command to reload the emotes ext

---
+++ emote_collector/extensions/admin.py
@@ -0,0 +1,17 @@
+from discord.ext import commands
+
+class Admin:
+	async def __local_check(self, context):
+		return await context.bot.is_owner(context.author)
+
+	@commands.command(name='reload-emotes', hidden=True)
+	async def reload_emotes(self, context):
+		replies = context.bot.get_cog('Emotes').replies
+		extension = 'emote_collector.extensions.emote'
+		context.bot.unload_extension(extension)
+		context.bot.load_extension(extension)
+		context.bot.get_cog('Emotes').replies = replies
+		await context.send(_('Reloaded the emotes extension with {} replies.').format(len(replies)))
+
+def setup(bot):
+	bot.add_cog(Admin())
