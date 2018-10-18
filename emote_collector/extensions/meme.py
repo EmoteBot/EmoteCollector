938f7cf1c06166ff1b21d045e02ee91596c7ab99
Benjamin Mintz <bmintz@protonmail.com>
2018-10-18 20:32:21 +0000

    simplify extensions/meme

--- emote_collector/extensions/meme.py
+++ emote_collector/extensions/meme.py
@@ -13,16 +13,14 @@ MEMES_FILE = os.path.join(BASE_DIR, 'data', 'memes.py')
 class Meme:
 	def __init__(self, bot):
 		self.bot = bot
-		self.read_memes()
-
-	def read_memes(self):
 		self.memes = utils.load_json_compat(MEMES_FILE)
 
 	@commands.command(hidden=True)
 	async def meme(self, context, *, name):
-		response = self.memes.get(name)
-		if response is not None:
-			await context.send(utils.fix_first_line(response))
+		try:
+			await context.send(utils.fix_first_line(self.memes[name]))
+		except KeyError:
+			pass
 
 def setup(bot):
 	if os.path.isfile(MEMES_FILE):
