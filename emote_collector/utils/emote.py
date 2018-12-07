fc9be10a095a6f458d70bec2f83a2bb49e612f4f
Benjamin Mintz <bmintz@protonmail.com>
2018-12-07 04:25:28 +0000

    utils/emote: fix emote url func
    ?v=1 actually causes some previews not to load now

--- emote_collector/utils/emote.py
+++ emote_collector/utils/emote.py
@@ -8,13 +8,14 @@ various utilities related to custom emotes and regular emojis
 import os.path
 import json
 
+import discord
+
 from .. import BASE_DIR
 
 with open(os.path.join(BASE_DIR, 'data', 'discord-emoji-shortcodes.json')) as f:
-	emoji_shortcodes = set(json.load(f))
+	emoji_shortcodes = frozenset(json.load(f))
 del f
 
 def url(id, *, animated: bool = False):
 	"""Convert an emote ID to the image URL for that emote."""
-	extension = 'gif' if animated else 'png'
-	return f'https://cdn.discordapp.com/emojis/{id}.{extension}?v=1'
+	return discord.PartialEmoji(animated=animated, name='', id=id).url
