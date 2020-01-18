d38534743b5274f33c0a1c535f609db7eac88a86
Io Mintz <io@mintz.cc>
2020-01-18 00:31:39 +0000

    bingo.sql: remove unused upsert_board query
    I must have missed this during the normalization

--- emote_collector/sql/bingo.sql
+++ emote_collector/sql/bingo.sql
@@ -23,17 +23,6 @@ ORDER BY RANDOM()
 LIMIT $1
 -- :endmacro
 
--- :macro upsert_board()
--- params: user_id, value, categories, marks
-INSERT INTO bingo_boards (user_id, value, categories, marks)
-VALUES ($1, $2, $3, $4)
-ON CONFLICT (user_id) DO UPDATE SET
-	value = EXCLUDED.value,
-	categories = EXCLUDED.categories,
-	marks = EXCLUDED.marks
-RETURNING value, categories, marks
--- :endmacro
-
 -- :macro delete_board()
 -- params: user_id
 DELETE FROM bingo_boards
