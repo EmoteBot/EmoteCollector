EmoteBot/EmoteCollector

commit 89eae2a4648968e43265b2f5639d3d56d6d7b1ac

ec/info: show full-size image in the embed

emote_collector/extensions/emote.py
@@ -89,9 +89,10 @@ class Emotes(commands.Cog):
 		The emote must be in the database.
 		"""
 		embed = discord.Embed()
-		embed.url = str(emote.url)
+		embed.url = emote.url
+		embed.set_thumbnail(url=emote.url)
 
-		embed.title = emote.status()
+		embed.title = emote.name
 
 		if emote.description is not None:
 			embed.description = emote.description
@@ -116,7 +117,7 @@ class Emotes(commands.Cog):
 				# hangul filler prevents the embed fields from jamming next to each other
 				value=utils.format_time(emote.modified) + '\N{hangul filler}')
 
-		embed.add_field(name='Usage count', value=await self.db.get_emote_usage(emote))
+		embed.add_field(name=_('Usage count'), value=await self.db.get_emote_usage(emote))
 
 		await context.send(embed=embed)
 
@@ -613,7 +614,7 @@ class Emotes(commands.Cog):
 			args.append(user.id)
 
 		processed = [
-			emote.status(linked=True)
+			emote.with_status(linked=True)
 			async for emote in self.db.all_emotes(*args, allow_nsfw=context.channel)]
 
 		if not processed:
@@ -634,7 +635,7 @@ class Emotes(commands.Cog):
 		"""Search for emotes whose name contains "query"."""
 
 		processed = [
-			emote.status(linked=True)
+			emote.with_status(linked=True)
 			async for emote in self.db.search(query, allow_nsfw=context.channel)]
 
 		if not processed:

emote_collector/extensions/db.py
@@ -81,24 +81,29 @@ class DatabaseEmote:
 		# we want to display :grinning:, not 😁
 		return fr'\:{self.name}:'
 
+	def linked_name(self):
+		return f'[{self.escaped_name()}]({self.url})'
+
 	def with_name(self):
 		"""return this emote as a string suitable for displaying in a list form or embed"""
 		return f'{self} {self.escaped_name()}'
 
 	def with_linked_name(self, *, separator='|'):
 		"""return this emote as a string suitable for displaying in a list form or embed"""
-		return f'{self} {separator} [{self.escaped_name()}]({self.url})'
+		return f'{self} {separator} {self.linked_name()}'
 
-	def status(self, *, linked=False):
-		formatted = self.with_linked_name() if linked else self.with_name()
-
+	def status(self):
 		if self.preserve and self.is_nsfw:
-			return _('{emote} (Preserved, NSFW)').format(emote=formatted)
+			return _('(Preserved, NSFW)')
 		if self.preserve and not self.is_nsfw:
-			return _('{emote} (Preserved)').format(emote=formatted)
+			return _('(Preserved)')
 		if not self.preserve and self.is_nsfw:
-			return _('{emote} (NSFW)').format(emote=formatted)
-		return formatted
+			return _('(NSFW)')
+		return ''
+
+	def with_status(self, *, linked=False):
+		formatted = self.with_linked_name() if linked else self.with_name()
+		return f'{formatted} {self.status()}'
 
 	@property
 	def url(self):

emote_collector/utils/misc.py
@@ -260,7 +260,6 @@ async def gather_or_cancel(*awaitables, loop=None):
 	def cancel_children(_):
 		for fut in futs:
 			fut.cancel()
-		# damn cancel culture killing children
 
 	gather_task.add_done_callback(cancel_children)
 	return await gather_task

commit 4ae15774a81948e392ec7a047ff48d8194badd48

ec/info: show status again
fixes a regression caused by 89eae2a4648968e43265b2f5639d3d56d6d7b1ac

emote_collector/extensions/emote.py
@@ -92,7 +92,7 @@ class Emotes(commands.Cog):
 		embed.url = emote.url
 		embed.set_thumbnail(url=emote.url)
 
-		embed.title = emote.name
+		embed.title = f'{emote.name} {emote.status()}'
 
 		if emote.description is not None:
 			embed.description = emote.description